099230f1df99e84bda691a4c93054348b5500945
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-09-26 01:57:03 +0530

    added image

--- src/Components/ImageEditor.jsx
+++ src/Components/ImageEditor.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
 const ImageEditor = () => {
-  return <div>Editor</div>;
+  return (
+    <div className="section">
+      <h2>Image Editor</h2>
+      <div className="image-area">
+        <img
+          src="https://images.drive.com.au/driveau/image/upload/c_fill,f_auto,g_auto,h_1080,q_auto:eco,w_1920/v1/cms/uploads/vaomydzwzbavlxugyp6e"
+          alt="image"
+        />
+      </div>
+      <div className="editor-area">
+        <div className="filter greyscale">Tokyo</div>
+        <div className="filter hue">Paris</div>
+        <div className="filter invert">Dublin</div>
+        <div className="filter contrast">Merlin</div>
+        <div className="filter brightness">Twuvo</div>
+      </div>
+    </div>
+  );
 };
 
 export default ImageEditor;
